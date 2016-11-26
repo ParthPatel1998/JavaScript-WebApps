ae588adcbfa6278208728005f1ce58d3a7195d05
Parth Patel <parth.patel_1998@hotmail.com>
2016-11-26 00:18:54 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var red = [0, 100, 63];
+var orange = [40, 100, 60];
+var green = [75, 100, 40];
+var blue = [196, 77, 55];
+var purple = [280, 50, 60];
+
+var colors = [red, orange, green, blue, purple];
+
+var myName = prompt("What's your name?");
+drawName(myName, colors);
+bubbleShape = 'circle';
+bounceBubbles();
